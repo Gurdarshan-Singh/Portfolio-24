8602aa80ed5197193a8f78970c0bd0ba47205cf6
Gurdarshan-Singh <gurdarshan408@gmail.com>
2024-08-25 00:31:37 -0400

    Footer Change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Typography } from '@mui/material'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+        <Typography variant='h5'>
+            Footer
+        </Typography>
+    </div>
   )
 }
